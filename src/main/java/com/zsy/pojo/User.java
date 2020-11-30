9b68782f94697e89e6971a78b9ff411b1ee11f2b
752391183@qq.com <yqdhgqd999>
2020-11-30 15:54:29 +0800

    init gittest

---
+++ src/main/java/com/zsy/pojo/User.java
@@ -0,0 +1,8 @@
+package com.zsy.pojo;
+
+/**
+ * @author zsy
+ * @date 2020/11/30 0030 - 15:46
+ */
+public class User {
+}
---
+++ src/test/java/com/zsy/test/UserTest.java
@@ -0,0 +1,8 @@
+package com.zsy.test;
+
+/**
+ * @author zsy
+ * @date 2020/11/30 0030 - 15:46
+ */
+public class UserTest {
+}
